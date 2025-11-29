1f2be15a03317561cc568c514e03632c5d2cbcf3
Sara.G <151964803+SaraGaire@users.noreply.github.com>
2025-11-29 19:19:43 +0100

    Create queue.constants.ts

---
+++ backend/src/jobs/queue.constants.ts
@@ -0,0 +1,4 @@
+export const DAILY_OUTFIT_QUEUE = 'daily-outfit';
+export const CLUSTERING_QUEUE = 'clustering';
+export const PRICE_TRACKING_QUEUE = 'price-tracking';
+export const ANALYTICS_QUEUE = 'analytics';
